87ea2d1fd7c4a5fea4d6b96f4a454a3ece07265b
Tom Brooks <d3vkit@gmail.com>
2023-01-12 16:51:34 -0800

    f

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, :type, presence: true
+  validates :type, inclusion: { in: %w[coach student] }
 end
