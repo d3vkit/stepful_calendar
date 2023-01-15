4cc469855ba539d9c6c2bc7f36a378dec07fe226
Tom <323595+d3vkit@users.noreply.github.com>
2023-01-15 12:09:10 -0800

    [11] Add availabilities to view (#13)
    * [11] Add availabilities to view

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -9,13 +9,24 @@ class AvailabilitiesController < ApplicationController
     @availability = calendar.availabilities.new
   end
 
-  def edit; end
+  def edit
+    @availability = Availability.find(params[:id])
+  end
 
-  def create; end
+  def create
+    @availability = calendar.availabilities.new(create_params)
+
+    create_and_respond(@availability)
+  end
 
   def update; end
 
-  def destroy; end
+  def destroy
+    @availability = Availability.find(params[:id])
+    @availability.destroy
+
+    redirect_to root_path, status: :see_other, notice: 'Availability was successfully created.'
+  end
 
   private
 
@@ -25,5 +36,21 @@ class AvailabilitiesController < ApplicationController
 
   def calendar
     @calendar ||= user.calendar
+  end
+
+  def create_params
+    params.require(:availability).permit(:start_time, :end_time)
+  end
+
+  def create_and_respond(availability)
+    respond_to do |format|
+      if availability.save
+        format.html { redirect_to user_calendar_url(user), notice: 'Availability was successfully created.' }
+        format.json { render :show, status: :created, location: calendar }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: availability.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
